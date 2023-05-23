cebd510aab70db032653a116f8a3c4f0c1991194
BTELNYY <btelnyy@gmail.com>
2023-05-22 20:52:36 -0600

    First commit.

---
+++ BetterSL/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterSL
+{
+    public class Config
+    {
+        [Description("Enable the plugin?")]
+        public bool PluginEnabled { get; set; } = true;
+
+        [Description("How much health should 106 start with?")]
+        public float Scp106StarterHealth { get; set; } = 1800f;
+    }
+}
---
+++ BetterSL/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginAPI;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Core;
+using PluginAPI.Helpers;
+
+namespace BetterSL
+{
+    public class Plugin
+    {
+        public const string PluginName = "BetterSL";
+        public const string PluginVersion = "0.1.0-alpha";
+        public const string PluginDesc = "A plugin to make \'better\' balancing changes to SL.";
+
+        public static Plugin instance;
+        [PluginConfig(PluginName + ".yml")]
+        public Config config = new Config();
+        public EventHandler eventHandler;
+
+        public string AudioPath = Path.Combine($"{Paths.Plugins}", "Sounds");
+
+        [PluginEntryPoint(PluginName, PluginVersion, PluginDesc, "btelnyy#8395")]
+        public void LoadPlugin()
+        {
+            if (!config.PluginEnabled)
+            {
+                Log.Debug("Plugin is disabled!");
+                return;
+            }
+            instance = this;
+            PluginAPI.Events.EventManager.RegisterEvents<EventHandler>(this);
+            Log.Debug("BetterSL v" + PluginVersion + " loaded.");
+        }
+
+        [PluginUnload()]
+        public void Unload()
+        {
+            config = null;
+            eventHandler = null;
+        }
+    }
+}
