e8b751f3d8b04e4d5b80a1392a3c1d61a24a8837
webmsgr <zapcontact000@gmail.com>
2023-05-28 21:29:07 -0700

    hume 106

---
+++ BetterSL/Patches/Hume106.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using PlayerRoles;
+using PlayerRoles.PlayableScps.HumeShield;
+using PlayerRoles.PlayableScps.Scp106;
+
+namespace BetterSL.Patches
+{
+    [HarmonyPatch(typeof(DynamicHumeShieldController), nameof(DynamicHumeShieldController.HsMax), MethodType.Getter)]
+    public class Hume106
+    {
+        static void Postfix(DynamicHumeShieldController __instance, ref float __result) 
+        {
+            if (__instance.Role is Scp106Role)
+            {
+                __result = ((__result - 300) / 600) * 400 + 300;
+            }
+        }
+    }
+}
