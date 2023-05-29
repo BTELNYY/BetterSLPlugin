d0087d124020e9b14673698b310539c48bc9ac30
webmsgr <zapcontact000@gmail.com>
2023-05-28 20:34:30 -0700

    jetbrains sucks it didnt commit the most important file

---
+++ BetterSL/Patches/SpookyZombieStareSpeedPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using PlayerRoles.PlayableScps.Scp049.Zombies;
+
+namespace BetterSL.Patches
+{ 
+    [HarmonyPatch(typeof(ZombieMovementModule), "UpdateSpeed")]
+    public class SpookyZombieStareSpeedPatch
+    {
+        static void Prefix(ref float ___BloodlustSpeed)
+        {
+            ___BloodlustSpeed = Plugin.instance.config.LobotomizedBloodlustMaxSpeed; // wow guys
+        }
+    }
+}
