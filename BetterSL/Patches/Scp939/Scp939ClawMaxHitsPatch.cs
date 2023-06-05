cc4f4e40337a73fc84cb4c5cb32091089cde88de
BTELNYY <btelnyy@gmail.com>
2023-06-04 21:22:51 -0600

    Scp 939 FIXED player AOE swings

--- BetterSL/Patches/Scp939/Scp939ClawMaxHitsPatch.cs
+++ BetterSL/Patches/Scp939/Scp939ClawMaxHitsPatch.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using PluginAPI.Core;
 using PlayerRoles.PlayableScps.Subroutines;
+using System.Runtime.CompilerServices;
 
 namespace BetterSL.Patches.Scp939
 {
@@ -42,43 +43,53 @@ namespace BetterSL.Patches.Scp939
             MethodInfo onDestructibleDamaged = AccessTools.Method(typeof(ScpAttackAbilityBase<Scp939Role>), "OnDestructibleDamaged");
             Type[] types = { typeof(bool) };
             MethodInfo serverSendRpc = AccessTools.Method(typeof(ScpAttackAbilityBase<Scp939Role>), "ServerSendRpc", types);
-            //Log.Debug("Got vars!");
-            int num = Physics.OverlapSphereNonAlloc(overlapSphereOrigin, detectionRadius, detectionsNonAlloc, detectionMask);
-            syncAttack = AttackResult.None;
-            //Log.Debug("Got intersects!");
-            int counter = 0;
-            for (int i = 0; i < num; i++)
+            while(targettedPlayers.Count > Plugin.GetConfig().Scp939MaxAoePlayerHits)
             {
-                //Log.Debug("iterating!");
-                IDestructible destructible;
-                if (detectionsNonAlloc[i].TryGetComponent<IDestructible>(out destructible) && !Physics.Linecast(plyCam.position, destructible.CenterOfMass, blockerMask))
-                {
-                    HitboxIdentity hitboxIdentity = destructible as HitboxIdentity;
-                    if ((hitboxIdentity == null || targettedPlayers.Remove(hitboxIdentity.TargetHub)) && destructible.Damage(__instance.DamageAmount, damageHandler, destructible.CenterOfMass))
-                    {
-                        object[] args = { destructible };
-                        onDestructibleDamaged.Invoke(__instance, args);
-                        syncAttack |= AttackResult.AttackedObject;
-                        HitboxIdentity hitboxIdentity2 = destructible as HitboxIdentity;
-                        if (hitboxIdentity2 != null)
-                        {
-                            if(counter >= Plugin.GetConfig().Scp939MaxAoePlayerHits)
-                            {
-                                break;
-                            }
-                            counter++;
-                            syncAttack |= AttackResult.AttackedHuman;
-                            if (hitboxIdentity2.TargetHub.playerStats.GetModule<HealthStat>().CurValue <= 0f)
-                            {
-                                syncAttack |= AttackResult.KilledHuman;
-                            }
-                        }
-                    }
-                }
+                System.Random rand = new System.Random();
+                int randomNumber = rand.Next(0, targettedPlayers.Count);
+                ReferenceHub hubtoremove = targettedPlayers.ToList()[randomNumber];
+                targettedPlayers.Remove(hubtoremove);
+                //targettedPlayers.Remove(targettedPlayers.Last());
             }
-            object[] args1 = { true };
-            serverSendRpc.Invoke(__instance, args1);
-            return false;
+            //Log.Debug("Got vars!");
+            //int num = Physics.OverlapSphereNonAlloc(overlapSphereOrigin, detectionRadius, detectionsNonAlloc, detectionMask);
+            //syncAttack = AttackResult.None;
+            //Log.Debug("Got intersects!");
+            //int counter = 0;
+            //for (int i = 0; i < num; i++)
+            //{
+            //    //Log.Debug("iterating!");
+            //    IDestructible destructible;
+            //    if (detectionsNonAlloc[i].TryGetComponent<IDestructible>(out destructible) && !Physics.Linecast(plyCam.position, destructible.CenterOfMass, blockerMask))
+            //    {
+            //        HitboxIdentity hitboxIdentity = destructible as HitboxIdentity;
+            //        if ((hitboxIdentity == null || targettedPlayers.Remove(hitboxIdentity.TargetHub)) && counter < Plugin.GetConfig().Scp939MaxAoePlayerHits && destructible.Damage(__instance.DamageAmount, damageHandler, destructible.CenterOfMass))
+            //        {
+            //            object[] args = { destructible };
+            //            onDestructibleDamaged.Invoke(__instance, args);
+            //            syncAttack |= AttackResult.AttackedObject;
+            //            HitboxIdentity hitboxIdentity2 = destructible as HitboxIdentity;
+            //            if (hitboxIdentity2 != null)
+            //            {
+            //                //if(counter >= Plugin.GetConfig().Scp939MaxAoePlayerHits)
+            //                //{
+            //                //    break;
+            //                //}
+            //                counter++;
+            //                //Hitmarker.SendHitmarker(__instance.Owner, 1f);
+            //                syncAttack |= AttackResult.AttackedHuman;
+            //                if (hitboxIdentity2.TargetHub.playerStats.GetModule<HealthStat>().CurValue <= 0f)
+            //                {
+            //                    syncAttack |= AttackResult.KilledHuman;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //object[] args1 = { true };
+            //serverSendRpc.Invoke(__instance, args1);
+            //return false;
+            return true;
         }
     }
 }
